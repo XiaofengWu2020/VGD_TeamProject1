4dfd1c77f1d7b2771568b37bdb02f0519d3d2738
Willzhang13 <93550693+Willzhang13@users.noreply.github.com>
2023-02-12 21:30:22 -0500

    bullet

---
+++ Assets/Bullet.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,13 +15,20 @@ public class PlayerController : MonoBehaviour
 	private float movementY;
 
 	private Rigidbody rb;
-	
+
+	public Transform bulletSpawnPoint;
+	public GameObject bulletPrefab;
+	public float bulletSpeed = 6;
+	private int shootCD = 100;
+	private bool shootable;
+
 
 	// At the start of the game..
 	void Start()
 	{
 		// Assign the Rigidbody component to our private rb variable
 		rb = GetComponent<Rigidbody>();
+		shootable = true;
 
 	}
 
@@ -32,6 +39,18 @@ public class PlayerController : MonoBehaviour
 
 		rb.AddForce(movement * speed);
 
+		if (Input.GetMouseButton(0) & shootable)
+		{
+			shootable = false;
+			var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
+			bullet.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 1f, 1.5f) * bulletSpeed, ForceMode.Impulse);
+		}
+		shootCD--;
+		if (shootCD < 0)
+		{
+			shootable = true;
+			shootCD = 100;
+		}
 	}
 	void Update()
 	{
