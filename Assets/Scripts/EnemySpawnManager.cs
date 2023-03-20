d99858e394fc8795f5b4becd7f5e620525d26891
Rewinder73 <ethanxboone@gmail.com>
2023-03-19 20:18:57 -0400

    Merged all Alpha features

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemySpawnManager : MonoBehaviour
 {
@@ -17,7 +18,14 @@ public class EnemySpawnManager : MonoBehaviour
     void SpawnEnemy()
     {
         int spawnindex = random.Next(0, enemySpawnPoint.Length - 1);
-        Instantiate(EnemyModel, enemySpawnPoint[spawnindex].transform.position, Quaternion.identity);
+        //Instantiate(EnemyModel, enemySpawnPoint[spawnindex].transform.position, Quaternion.identity);
+
+        // For Alpha, use NavMeshHit spawning below:
+        NavMeshHit closestHit;
+        Vector3 spawn = enemySpawnPoint[spawnindex].transform.position;
+        if (NavMesh.SamplePosition(spawn, out closestHit, 500f, NavMesh.AllAreas))
+            spawn = new Vector3(closestHit.position.x, closestHit.position.y + 20, closestHit.position.z);
+        Instantiate(EnemyModel, spawn, Quaternion.identity);
     }
 
 }
